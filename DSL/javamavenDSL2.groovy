c15b47988bd87233fca9295166e90255d21303a8
LibrerosDavid <89157243+LibrerosDavid@users.noreply.github.com>
2022-10-20 12:00:38 -0500

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,18 +1,18 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/LibrerosDavid/simple-java-maven-app.git', 'master') { node ->
+		node / gitConfigName('LibrerosDavid')
+		node / gitConfigEmail('j.d.libreroslopez@gmail.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven jenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven jenkins')
           goals('test')
         }
         shell('''
